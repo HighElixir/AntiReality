8295dbc3cfa7e93bbfa867ac9caa6c03263a0815
huna1379128479847 <human.kagoshima.hunagumayuuhi@gmail.com>
2024-08-30 23:47:02 +0900

    テレポート作業

---
+++ 1.5/Assemblies/CS files/Abilitys/Teleport/HediffComp_TransportGizmo.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using System;
+
+namespace HE_AntiReality
+{
+    public class HediffComp_TransportGizmo : HediffComp
+    {
+        private int countTick = 0; // 最後にテレポートを使用したゲーム内時間（Tick）
+        private const int CooldownTicks = 6000; // クールダウンの時間（600 ticks = 10秒）
+        private const float MaxTeleportDistance = 20f; // テレポートの最大距離
+
+        public HediffCompProperties_TransportGizmo Props
+        {
+            get
+            {
+                return (HediffCompProperties_TransportGizmo)props;
+            }
+        }
+
+        public override IEnumerable<Gizmo> CompGetGizmos()
+        {
+            Command_Action teleportCommand = new Command_Action
+            {
+                defaultLabel = "座標替え",
+                defaultDesc = "視界の通る範囲内に座標を変える",
+                icon = ContentFinder<Texture2D>.Get("UI/Commands/Teleport"),
+                action = () =>
+                {
+                    if (countTick > 0)
+                    {
+                        Messages.Message("クールダウン中。あと " + ((float)countTick / 60f).ToString("0.0") + " 秒", MessageTypeDefOf.RejectInput, false);
+                    }
+                    else
+                    {
+                        TeleportTargetingSource targetingSource = new TeleportTargetingSource(parent.pawn, MaxTeleportDistance);
+
+                        Find.Targeter.BeginTargeting(targetingSource.targetParams, OnTargetSelected(targetingSource));
+                    }
+                }
+            };
+
+            // クールダウン中は無効化
+            if (countTick != 0)
+            {
+                teleportCommand.Disable("クールダウン中です");
+            }
+
+            yield return teleportCommand;
+        }
+
+        private Action<LocalTargetInfo> OnTargetSelected(TeleportTargetingSource targetingSource)
+        {
+            return target =>
+            {
+                if (targetingSource.ValidateTarget(target))
+                {
+                    targetingSource.OrderForceTarget(target);
+                    countTick = CooldownTicks; // 使用時間を更新
+                }
+            };
+        }
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            if (countTick > 0)
+            {
+                countTick--;
+            }
+            if (countTick < 0)
+            {
+                countTick = 0;
+            }
+        }
+    }
+}
---
+++ 1.5/Assemblies/CS files/Abilitys/Teleport/TeleportTargetingSource.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace HE_AntiReality
+{
+    public class TeleportTargetingSource : ITargetingSource
+    {
+        private Pawn casterPawn;
+        private float maxDistance;
+
+        public TeleportTargetingSource(Pawn pawn, float maxDistance)
+        {
+            this.casterPawn = pawn;
+            this.maxDistance = maxDistance;
+        }
+
+        public bool IsCollect { get; set; }
+        public bool CasterIsPawn => true;
+
+        public bool IsMeleeAttack => false;
+
+        public bool Targetable => true;
+
+        public bool MultiSelect => false;
+
+        public bool HidePawnTooltips => false;
+
+        public Thing Caster => casterPawn;
+
+        public Pawn CasterPawn => casterPawn;
+
+        public Verb GetVerb => null; // テレポートは直接の攻撃動作ではないため、null
+
+        public Texture2D UIIcon => ContentFinder<Texture2D>.Get("UI/Commands/Teleport");
+
+        public TargetingParameters targetParams => TargetingParameters_Extensions.ForTeleport(casterPawn, maxDistance);
+
+        public ITargetingSource DestinationSelector => null;
+
+        public bool CanHitTarget(LocalTargetInfo target)
+        {
+            // 距離と視界の判定
+            return target.Cell.IsValid
+                && casterPawn.Position.InHorDistOf(target.Cell, maxDistance)
+                && GenSight.LineOfSight(casterPawn.Position, target.Cell, casterPawn.Map);
+        }
+
+        public bool ValidateTarget(LocalTargetInfo target, bool showMessages = true)
+        {
+            if (CanHitTarget(target))
+            {
+                return true;
+            }
+
+            if (showMessages)
+            {
+                Messages.Message("範囲外", MessageTypeDefOf.RejectInput, false);
+            }
+            return false;
+        }
+
+        public void DrawHighlight(LocalTargetInfo target)
+        {
+            GenDraw.DrawRadiusRing(casterPawn.Position, maxDistance);
+        }
+
+        public void OrderForceTarget(LocalTargetInfo target)
+        {
+            if (ValidateTarget(target))
+            {
+                casterPawn.Position = target.Cell;
+                casterPawn.Notify_Teleported(true, true);
+                IsCollect = true;
+            }
+        }
+
+        public void OnGUI(LocalTargetInfo target)
+        {
+            // 必要に応じて、ターゲット選択時にGUIで何かを描画する場合に実装
+        }
+    }
+}
